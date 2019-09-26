bd4f72403d3b6992c38c22319c339b09a09b704e
Relecteve <Filonovaval@gmail.com>
2019-09-26 21:39:37 +0300

    Сделал исправление от наставника

--- js/main.js
+++ js/main.js
@@ -5,7 +5,20 @@ var CHECKIN_HOUSE = ['12:00', '13:00', '14:00'];
 var CHECKOUT_HOUSE = ['12:00', '13:00', '14:00'];
 var FEATURES_HOUSE = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var TITLE_HOUSE = ['Полуразволившейся сарай', 'Шикарный пентхаус на крыше пятиэтажки', 'Квартира для праздников'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+var MIN_ROOMS = 1;
+var MAX_ROOMS = 10;
+var MIN_GUETS = 1;
+var MAX_GUETS = 10;
+var MIN_PRICE = 1000;
+var MAX_PRICE = 1000000;
+var PINS = 8;
+var MIN_Y_PIN = 130;
+var MAX_Y_PIN = 630;
+var MIN_X_PIN = 10;
+var MAX_X_PIN = 1120;
+var PHOTOS = ['/assets/images/tokyo/hotel1.jpg', '/assets/images/tokyo/hotel2.jpg', '/assets/images/tokyo/hotel3.jpg'];
 var map = document.querySelector('.map');
 // var similarHouseTemplate = document.querySelector("#card").content.querySelector(".map__card");
 var similarPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -20,8 +33,8 @@ var getRandomElement = function (elements) {
 };
 var createRandomAd = function (index) {
   var location = {
-    x: getRandomInt(10, 1140),
-    y: getRandomInt(130, 630)
+    x: getRandomInt(MIN_X_PIN, MAX_X_PIN),
+    y: getRandomInt(MIN_Y_PIN, MAX_Y_PIN)
   };
   return {
     author: {
@@ -34,10 +47,10 @@ var createRandomAd = function (index) {
     offer: {
       title: getRandomElement(TITLE_HOUSE),
       address: location.x + ', ' + location.y,
-      price: getRandomInt(1000, 1000000),
+      price: getRandomInt(MIN_PRICE, MAX_PRICE),
       type: getRandomElement(TYPE_HOUSE),
-      rooms: getRandomInt(1, 10),
-      guests: getRandomInt(1, 10),
+      rooms: getRandomInt(MIN_ROOMS, MAX_ROOMS),
+      guests: getRandomInt(MIN_GUETS, MAX_GUETS),
       checkin: getRandomElement(CHECKIN_HOUSE),
       checkout: getRandomElement(CHECKOUT_HOUSE),
       features: getRandomElement(FEATURES_HOUSE),
@@ -48,7 +61,7 @@ var createRandomAd = function (index) {
 
 var createRandomAds = function () {
   var houses = [];
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < PINS; i++) {
     houses[i] = createRandomAd(i);
   }
   return houses;
@@ -59,7 +72,8 @@ var renderPinHouse = function (house) {
 
   housePinElement.querySelector('img').src = house.author.avatar;
   housePinElement.querySelector('img').alt = house.offer.title;
-  housePinElement.style = ('left: ' + house.location.x + 'px; top: ' + house.location.y + 'px;');
+  housePinElement.style.left = (house.location.x + PIN_WIDTH / 2) + 'px';
+  housePinElement.style.top = house.location.y + PIN_HEIGHT + 'px';
   return housePinElement;
 };
 
