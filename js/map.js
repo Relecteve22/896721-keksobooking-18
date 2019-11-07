e818a5a1bf1e8734550ba11c53f8ab93c7bcd687
relecteve <Filonovaval@gmail.com>
2019-11-07 20:50:47 +0300

    Делаю фильтрацию

--- js/map.js
+++ js/map.js
@@ -9,15 +9,9 @@
     WIDTH: 50,
     HEIGHT: 70
   };
-
-  var MyPin = {
-    WIDTH: 62,
-    HEIGHT: 82
-  };
   var MAX_NUMBER_PINS = 5;
 
   var map = document.querySelector('.map');
-  var myPin = document.querySelector('.map__pin--main');
   var mapFiltersForm = document.querySelector('.map__filters');
   var similarPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
@@ -27,7 +21,6 @@
   var mapWidth = map.offsetWidth;
   var allAds = [];
   var sharpMarkX = Pin.WIDTH / 2;
-  var mysharpMarkX = MyPin.WIDTH / 2;
 
   var renderPinHouse = function (house) {
     var housePinElement = similarPinTemplate.cloneNode(true);
@@ -50,6 +43,7 @@
     }
     return map.appendChild(fragment);
   };
+
   var activatePage = function () {
     map.classList.remove('map--faded');
     window.form.adForm.classList.remove('ad-form--disabled');
@@ -68,10 +62,22 @@
   cordinatesPinInputStart();
   window.form.houseTypeDoValidity(window.form.minPriceHouses);
 
+  var mapFilter = document.querySelector('.map__filters');
+  var selectFilterTypes = mapFilter.querySelector('#housing-type');
+
   var successHandler = function (ads) {
     allAds = ads;
-    renderHouses(allAds);
+    // renderHouses(allAds);
   };
+
+  selectFilterTypes.addEventListener('change', function () {
+    var filterMapForm = function () {
+      return allAds.filter(function (currentValue, index) {
+        return window.filter.isFilter(allAds[index]);
+      });
+    };
+    renderHouses(filterMapForm());
+  });
 
   var showModal = function () {
     var errorTempaltePopup = errorTemplate.cloneNode(true);
@@ -105,8 +111,8 @@
   var activeAndLoad = function () {
     activatePage();
     window.backend.load(successHandler, errorHandler);
-    myPin.removeEventListener('mousedown', myPinMouseDownHanlder);
-    myPin.removeEventListener('keydown', myPinKeydownHandler);
+    window.pin.myPin.removeEventListener('mousedown', myPinMouseDownHanlder);
+    window.pin.myPin.removeEventListener('keydown', myPinKeydownHandler);
   };
 
   var myPinMouseDownHanlder = function () {
@@ -119,97 +125,13 @@
     }
   };
 
-  myPin.addEventListener('mousedown', myPinMouseDownHanlder);
-  myPin.addEventListener('keydown', myPinKeydownHandler);
-
-  var getPinLeft = function (left) {
-    if (left < window.pin.MIN_X_PIN - mysharpMarkX) {
-      return window.pin.MIN_X_PIN - mysharpMarkX;
-    }
-
-    if ((left + MyPin.WIDTH) > mapWidth + mysharpMarkX) {
-      return (mapWidth + mysharpMarkX) - MyPin.WIDTH;
-    }
-
-    return left;
-  };
-
-  var getPinTop = function (top) {
-    if (top < window.pin.MIN_Y_PIN - MyPin.HEIGHT) {
-      return window.pin.MIN_Y_PIN - MyPin.HEIGHT;
-    }
-
-    if ((top + MyPin.HEIGHT) > window.pin.MAX_Y_PIN) {
-      return window.pin.MAX_Y_PIN - MyPin.HEIGHT;
-    }
-
-    return top;
-  };
-
-  var movePin = function (left, top) {
-    myPin.style.top = top + 'px';
-    myPin.style.left = left + 'px';
-  };
-
-  myPin.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-
-    var dragged = false;
-
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-      dragged = true;
-
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      var coodXLeftMyPin = myPin.offsetLeft - shift.x;
-      var coodYTopMyPin = myPin.offsetTop - shift.y;
-
-      var updateAddress = function (left, top) {
-        inputCordenatios.value = (left + Math.floor(mysharpMarkX)) + ', ' + (top + MyPin.HEIGHT);
-      };
-
-      var left = getPinLeft(coodXLeftMyPin);
-      var top = getPinTop(coodYTopMyPin);
-
-      movePin(left, top);
-
-      updateAddress(left, top);
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-
-      if (dragged) {
-        var onClickPreventDefault = function (defaultEvt) {
-          defaultEvt.preventDefault();
-          myPin.removeEventListener('click', onClickPreventDefault);
-        };
-        myPin.addEventListener('click', onClickPreventDefault);
-      }
-    };
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
+  window.pin.myPin.addEventListener('mousedown', myPinMouseDownHanlder);
+  window.pin.myPin.addEventListener('keydown', myPinKeydownHandler);
 
   window.map = {
     element: map,
-    mapFiltersForm: mapFiltersForm
+    mapFiltersForm: mapFiltersForm,
+    mapWidth: mapWidth,
+    inputCordenatios: inputCordenatios
   };
 })();
