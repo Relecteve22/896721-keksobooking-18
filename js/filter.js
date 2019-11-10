5ab1e2b30df961c987766b1b950c9ef062ae6719
relecteve <Filonovaval@gmail.com>
2019-11-10 17:31:13 +0300

    Практически сделал фильтрацию

--- js/filter.js
+++ js/filter.js
@@ -6,12 +6,22 @@
   var selectFilterPrices = mapFilter.querySelector('#housing-price');
   var selectFilterRooms = mapFilter.querySelector('#housing-rooms');
   var selectFilterGuets = mapFilter.querySelector('#housing-guests');
+  var featuresFilter = mapFilter.querySelector('#housing-features');
 
   var currentFilter = {
     'housing-type': 'any',
     'housing-price': 'any',
     'housing-rooms': 'any',
     'housing-guests': 'any'
+  };
+
+  var currentFeaturesFilter = {
+    'wifi': '',
+    'dishwasher': '',
+    'parking': '',
+    'washer': '',
+    'elevator': '',
+    'conditioner': ''
   };
 
   var isAny = function (value) {
@@ -22,16 +32,15 @@
     return isAny(currentValue) || value.toString() === currentValue;
   };
 
-  // var isCheckbox = function (value, currentValue) {
-  //   if (!value) {
-  //     return false;
-  //   }
-  //   for (var i = 0; i < value.length; i++) {
-  //     if (value[i] === currentValue) {
-  //       return true;
-  //     }
-  //   }
-  // };
+  var isCheckbox = function (value, currentValue) {
+    for (var i = 0; i < value.length; i++) {
+      if (value[i] === currentValue) {
+        return true;
+      }
+      return false;
+    }
+    return false;
+  };
 
   var isPrice = function (value, currentValue) {
     var minPrice = 10000;
@@ -53,14 +62,20 @@
     return false;
   };
 
-  // var checkboxsFilterChangeHandler = function (evt) {
+  var checkboxsFilterChangeHandler = function (evt) {
+    currentFeaturesFilter[evt.target.checked] = evt.target.value;
+    var allAds = window.map.returnAllAds();
+    var filteredPins = allAds.filter(function (ad) {
+      return isCheckbox(ad.offer.features, currentFeaturesFilter[true]);
+    });
 
-  // };
+    window.map.renderHouses(filteredPins);
+  };
 
   var selectFilterChangeHandler = function (evt) {
     currentFilter[evt.target.name] = evt.target.value;
-    var arr = window.map.returnAllAds();
-    var filteredPins = arr.filter(function (ad) {
+    var allAds = window.map.returnAllAds();
+    var filteredPins = allAds.filter(function (ad) {
       return is(ad.offer.type, currentFilter['housing-type'])
         && is(ad.offer.rooms, currentFilter['housing-rooms'])
         && is(ad.offer.guests, currentFilter['housing-guests'])
@@ -74,4 +89,5 @@
   selectFilterRooms.addEventListener('change', selectFilterChangeHandler);
   selectFilterGuets.addEventListener('change', selectFilterChangeHandler);
   selectFilterPrices.addEventListener('change', selectFilterChangeHandler);
+  featuresFilter.addEventListener('change', checkboxsFilterChangeHandler);
 })();
