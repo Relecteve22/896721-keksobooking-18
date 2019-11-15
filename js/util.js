56f8be34fadf8cfc8509802a6f0047e447d1dfc8
relecteve <Filonovaval@gmail.com>
2019-11-16 00:19:45 +0300

    Добавил комментарий

--- js/util.js
+++ js/util.js
@@ -3,7 +3,7 @@
 (function () {
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
-  var DEBOUNCE_INTERVAL = 500; // ms
+  var DEBOUNCE_INTERVAL = 500; // милисекунды
 
   var debounce = function (cb, interval) {
     var lastTimeout = null;
